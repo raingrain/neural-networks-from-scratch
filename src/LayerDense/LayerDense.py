bfe861c4b201bcd018d148fb498042a7cf0b0ac2
raingrain <2711835261@qq.com>
2024-02-09 19:14:27 +0800

    Update LayerDense.py

--- src/LayerDense/LayerDense.py
+++ src/LayerDense/LayerDense.py
@@ -4,15 +4,15 @@ import numpy as np
 # 全连接层 or 稠密层 or 致密层
 class Layer_Dense:
 
-    # 随机初始化
+    # 初始化
     def __init__(self, n_inputs, n_neurons):
-        # 随机初始化偏差和权重
+        # 随机初始化偏差
         # 初始化每一个神经元对各个输入的权重
         # 我们将把高斯分布的权重乘以0.01，以生成数值小数数量级的值
         # 否则在训练过程中，由于起始值相对于训练中进行的更新过大，模型会花费更多时间来适应数据
         # 这里的想法是以足够小的非零值启动模型，以确保它们不会对训练产生影响
         self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
-        # 初始化每一个神经元的偏差
+        # 初始化每一个神经元的偏差，将偏差设置为0
         self.biases = np.zeros((1, n_neurons))
         self.output = None
 
