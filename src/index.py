c7295ad516bdbe2490e1b8534586e19ccf5038a3
raingrain <2711835261@qq.com>
2024-02-11 16:28:41 +0800

    Update index.py

--- src/index.py
+++ src/index.py
@@ -2,53 +2,43 @@ import nnfs
 
 from nnfs.datasets import spiral_data
 
+from src.Accuracy.AccuracyCategorical import Accuracy_Categorical
 from src.Activation.ActivationReLU import Activation_ReLU
 from src.Activation.ActivationSoftmax import Activation_Softmax
 from src.Layer.LayerDense import Layer_Dense
+from src.Layer.LayerDropout import Layer_Dropout
 from src.Loss.LossCategoricalCrossentropy import Loss_CategoricalCrossentropy
+from src.Model import Model
+from src.Optimizer.OptimizerAdam import Optimizer_Adam
 
-# 锁定随机种子，使得数据固定便于调试
+# Fixed random seeds make data consistent
 nnfs.init()
 
-# 创建数据集
-X, y = spiral_data(samples=100, classes=3)
+# Create dataset
+X, y = spiral_data(samples=1000, classes=3)
+X_test, y_test = spiral_data(samples=100, classes=3)
 
-# 使用2个输入特征和3个输出值创建全连接层
-dense1 = Layer_Dense(2, 3)
+# Instantiate the model
+model = Model()
 
-# 创建ReLU激活（与密集层一起使用）：
-activation1 = Activation_ReLU()
+# Add layers
+model.add(
+    Layer_Dense(2, 512, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4))
+model.add(Activation_ReLU())
+model.add(Layer_Dropout(0.1))
+model.add(Layer_Dense(512, 3))
+model.add(Activation_Softmax())
 
-# 创建具有3个输入特征的第二个密集层（如我们在此处获取上一层的输出）和3个输出值
-dense2 = Layer_Dense(3, 3)
+# Set loss, optimizer and accuracy objects
+model.set(
+    loss=Loss_CategoricalCrossentropy(),
+    optimizer=Optimizer_Adam(learning_rate=0.05, decay=5e-5),
+    accuracy=Accuracy_Categorical()
+)
 
-# 创建Softmax激活（用于密集层）：
-activation2 = Activation_Softmax()
+# Finalize the model
+model.finalize()
 
-# 创建损失函数
-loss_function = Loss_CategoricalCrossentropy()
-
-# 通过该层向前传递我们的训练数据
-dense1.forward(X)
-
-# 继续前向传播
-# 这里取第一个稠密层的输出
-activation1.forward(dense1.output)
-
-# 继续前向传播
-# 它以第一层激活函数的输出作为输入
-dense2.forward(activation1.output)
-
-# 继续前向传播
-# 这里取第二个稠密层的输出
-activation2.forward(dense2.output)
-
-# 让我们看看前几个示例的输出：
-print(activation2.output[:5])
-
-# 继续向前传播
-# 这里取第二个稠密层的输出并返回损耗
-loss = loss_function.calculate(activation2.output, y)
-
-# 打印损失值
-print('loss:', loss)
+# Train the model
+model.train(X, y, validation_data=(X_test, y_test), epochs=10000,
+            print_every=100)
