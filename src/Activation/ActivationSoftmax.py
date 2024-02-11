04cdd0983163213f2b343ba92bfc7ea8caae5bf6
raingrain <2711835261@qq.com>
2024-02-11 16:00:29 +0800

    Update ActivationSoftmax.py

--- src/Activation/ActivationSoftmax.py
+++ src/Activation/ActivationSoftmax.py
@@ -4,14 +4,39 @@ import numpy as np
 # Softmax activation
 class Activation_Softmax:
 
-    def __init__(self):
-        self.output = None
-
     # Forward pass
-    def forward(self, inputs):
-        # 对于每一行的一组数据，先减去最大值，然后计算e的次方
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        # 对每个样本进行归一化处理，总和标准化
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        # 输出
+    def forward(self, inputs, training):
+        # Remember input values
+        self.inputs = inputs
+
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                            keepdims=True))
+
+        # Normalize them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1,
+                                            keepdims=True)
+
         self.output = probabilities
+
+    # Backward pass
+    def backward(self, dvalues):
+        # Create uninitialized array
+        self.dinputs = np.empty_like(dvalues)
+
+        # Enumerate outputs and gradients
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1, 1)
+            # Calculate Jacobian matrix of the output
+            jacobian_matrix = np.diagflat(single_output) - \
+                              np.dot(single_output, single_output.T)
+            # Calculate sample-wise gradient
+            # and add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix,
+                                         single_dvalues)
+
+    # Calculate predictions for outputs
+    def predictions(self, outputs):
+        return np.argmax(outputs, axis=1)
