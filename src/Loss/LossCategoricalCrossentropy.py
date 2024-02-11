27d3fd98914f42dd19e6204d8025d54720c7d70a
raingrain <2711835261@qq.com>
2024-02-11 16:18:16 +0800

    Update LossCategoricalCrossentropy.py

--- src/Loss/LossCategoricalCrossentropy.py
+++ src/Loss/LossCategoricalCrossentropy.py
@@ -3,26 +3,52 @@ import numpy as np
 from src.Loss.Loss import Loss
 
 
-# 交叉熵损失
+# Cross-entropy loss
 class Loss_CategoricalCrossentropy(Loss):
 
-    # 前向传播
+    # Forward pass
     def forward(self, y_pred, y_true):
-        # 一个batch中有几个sample
+
+        # Number of samples in a batch
         samples = len(y_pred)
 
-        # 剪裁数据以防止被0除
-        # 剪裁两侧以不将均值拖向任何值
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
         y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
 
-        # 目标值的概率 - 仅当类别标签
+        # Probabilities for target values -
+        # only if categorical labels
         if len(y_true.shape) == 1:
-            correct_confidences = y_pred_clipped[range(samples), y_true]
+            correct_confidences = y_pred_clipped[
+                range(samples),
+                y_true
+            ]
 
-        # 掩码值 - 仅适用于一个热编码标签
+        # Mask values - only for one-hot encoded labels
         elif len(y_true.shape) == 2:
-            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+            correct_confidences = np.sum(
+                y_pred_clipped * y_true,
+                axis=1
+            )
 
-        # 损失值
+        # Losses
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
+
+    # Backward pass
+    def backward(self, dvalues, y_true):
+
+        # Number of samples
+        samples = len(dvalues)
+        # Number of labels in every sample
+        # We'll use the first sample to count them
+        labels = len(dvalues[0])
+
+        # If labels are sparse, turn them into one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+
+        # Calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
